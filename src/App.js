8fca548b468ec79fb28cead5789a818727305548
Torra Agustin <torra.agustinignacio@gmail.com>
2020-06-19 18:33:29 -0300

    update App component with counter app implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const decrementCounter = () => {
+    if (counter > 0) setCounter((prevCounter) => prevCounter - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter app</h1>
+      <p id="counter">{counter}</p>
+      <button id="btnIncrement" onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+      <button id="btnDecrement" onClick={decrementCounter}>
+        Decrement
+      </button>
     </div>
   );
 }
