755e12c543d2f2a805ce95291bc5b7fb857f49b3
Torra Agustin <torra.agustinignacio@gmail.com>
2020-06-19 18:32:44 -0300

    create tests for counter app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+// import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+describe("Counter testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  test("render the title of the counter", () => {
+    expect(wrapper.find("h1").text()).toContain("Counter app");
+  });
+
+  test("render a button with text Increment", () => {
+    expect(wrapper.find("#btnIncrement").text()).toBe("Increment");
+  });
+
+  test("render initial value", () => {
+    expect(wrapper.find("#counter").text()).toBe("0");
+  });
+
+  test("render click event of increment button and increment counter value by 1", () => {
+    wrapper.find("#btnIncrement").simulate("click");
+    expect(wrapper.find("#counter").text()).toBe("1");
+  });
+
+  test("render click event of decrement button and decrement counter by 1", () => {
+    wrapper.find("#btnIncrement").simulate("click");
+    expect(wrapper.find("#counter").text()).toBe("1");
+    wrapper.find("#btnDecrement").simulate("click");
+    expect(wrapper.find("#counter").text()).toBe("0");
+  })
+
+  test("that counter does not take negative numbers", () => {
+    wrapper.find("#btnDecrement").simulate("click");
+    expect(wrapper.find("#counter").text()).toBe("0");
+  })
 });
+
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
